04b145e3a4c012a29544b6c786e0ad1b3e6ac027
CodeCraftyWizard <144531468+CodeCraftyWizard@users.noreply.github.com>
2023-09-09 16:39:42 +0530

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+// Javascript -------------
+
+var card = document.querySelectorAll(".card");
+
+card[0].classList.add('active');
+
+card.forEach(function(expand){
+  expand.addEventListener('click', function (clickEvent) {
+    for (var img of card) {
+      img.classList.remove('active');
+    }
+    clickEvent.target.classList.add('active');
+  })
+})
